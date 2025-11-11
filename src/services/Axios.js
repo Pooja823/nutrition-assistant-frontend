5210e9ac32dd1ee91f4774340532861c2b4afed8
Pooja823 <pn262005@gmail.com>
2025-11-11 19:34:01 +0530

    Fix line endings

---
+++ src/services/Axios.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://nutrition-assistant-backend-production.up.railway.app',
+});
+
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token') || '';
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export const Axios = instance;
